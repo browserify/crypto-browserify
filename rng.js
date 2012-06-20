7a1001e106b529d726bfc83646ef0717458139e5
Derek P <derek@derekperez.com>
2012-06-19 23:11:57 -0700

    missing var for _global.

--- rng.js
+++ rng.js
@@ -1,7 +1,7 @@
 // Original code adapted from Robert Kieffer.
 // details at https://github.com/broofa/node-uuid
 (function() {
-  _global = this;
+  var _global = this;
 
   var mathRNG, whatwgRNG;
 
