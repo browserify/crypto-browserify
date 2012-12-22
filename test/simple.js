23b28f028f41d5e0d4141e2f5de581a3ae2b7476
Dominic Tarr <dominic.tarr@gmail.com>
2012-12-22 13:47:36 -0500

    port tests to tape

--- test/simple.js
+++ test/simple.js
@@ -1,29 +1,34 @@
+var test = require("tape")
+
 var crypto = require('crypto')
 var cryptoB = require('../')
 var assert = require('assert')
 
-
 function assertSame (fn) {
-  fn(crypto, function (err, expected) {
-    fn(cryptoB, function (err, actual) {
-      assert.equal(actual, expected)
+  test(fn.name, function (t) {
+    fn(crypto, function (err, expected) {
+      fn(cryptoB, function (err, actual) {
+        t.equal(actual, expected)
+        t.end()
+      })
     })
   })
 }
 
-assertSame(function (crypto, cb) {
+assertSame(function sha1 (crypto, cb) {
   cb(null, crypto.createHash('sha1').update('hello', 'utf-8').digest('hex'))
 })
 
-assertSame(function (crypto, cb) {
+assertSame(function md5(crypto, cb) {
   cb(null, crypto.createHash('md5').update('hello', 'utf-8').digest('hex'))
 })
 
 assert.equal(cryptoB.randomBytes(10).length, 10)
-
-cryptoB.randomBytes(10, function(ex, bytes) {
-  assert.ifError(ex)
-  bytes.forEach(function(bite) {
-    assert.equal(typeof bite, 'number')
-  });
+test('randomBytes', function (t) {
+  cryptoB.randomBytes(10, function(ex, bytes) {
+    assert.ifError(ex)
+    bytes.forEach(function(bite) {
+      assert.equal(typeof bite, 'number')
+    })
+  })
 })
