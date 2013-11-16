ec8fd643148050c2f9c3b1f7bb859d636c4a191f
Loren Segal <lsegal@amazon.com>
2013-11-15 19:16:44 -0800

    Add test vectors for hash algorithms
    Test algorithms against vector data from NSRL (NIST):
    
    http://www.nsrl.nist.gov/testdata/

--- test/node.js
+++ test/node.js
@@ -2,6 +2,7 @@ var test = require('tape');
 
 var crypto = require('crypto');
 var cryptoB = require('../');
+var fs = require('fs');
 
 function assertSame(name, fn) {
     test(name, function (t) {
@@ -43,6 +44,25 @@ algorithms.forEach(function (algorithm) {
     });
 });
 
+function pad(n, w) {
+  n = n + ''; return new Array(w - n.length + 1).join('0') + n;
+}
+
+var vectors = fs.readdirSync(__dirname + '/vectors').sort().
+    filter(function (t) { return t.match(/\.dat$/); }).
+    map(function (t) { return fs.readFileSync(__dirname + '/vectors/' + t); });
+
+['md5', 'sha1', 'sha256'].forEach(function (algorithm) {
+    test(algorithm, function (t) {
+        function hash(data) { return cryptoB.createHash(algorithm).update(data).digest('hex'); }
+
+        var hashes = fs.readFileSync(__dirname + '/vectors/byte-hashes.' + algorithm).toString().split(/\r?\n/);
+        t.plan(vectors.length);
+        for (var i = 0; i < vectors.length; i++) {
+            t.equal(hash(vectors[i]), hashes[i], 'byte' + pad(i, 4) + '.dat');
+        }
+    });
+});
 
 test('randomBytes', function (t) {
     t.plan(5);
