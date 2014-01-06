c50c320c1962f4e9f9bd2071da7ca31cad1da270
Dominic Tarr <dominic.tarr@gmail.com>
2014-01-06 17:15:21 +0700

    didn't use this in the end

--- test/util.js
+++ test/util.js
@@ -16,26 +16,3 @@ function assertSame(name, fn) {
     });
 }
 
-//iterate over a cartesian join
-exports.join = join
-
-function join (args, each) {
-  args = args.slice()
-    var first = args.shift()
-  if(args.length) {
-    first.forEach(function (v) {
-      join(args, function (vargs) {
-        each([v].concat(vargs))
-      })
-    })
-  }
-  else
-    first.forEach(function (v) {
-      each([v])
-    })
-}
-
-
-if(!module.parent) {
-  join([[1,2,3], ['a', 'b', 'c'], [true, false, null]], console.log)
-}
