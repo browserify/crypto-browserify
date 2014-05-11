f9ee3c0658875122e8856b14a2c6270ec254569e
Dominic Tarr <dominic.tarr@gmail.com>
2014-05-11 14:05:00 +0200

    test with published test vectors

--- test/create-hash.js
+++ test/create-hash.js
@@ -2,31 +2,32 @@ var assertSame = require('./util').same
 var fs = require('fs')
 var test = require('tape')
 
-var algorithms = ['sha1', 'sha256', 'md5', 'rmd160'];
+var algorithms = require('../').getHashes()
 var encodings = [/*'binary',*/ 'hex', 'base64'];
+var vectors = require('hash-test-vectors')
 
+var createHash = require('../create-hash')
 
 algorithms.forEach(function (algorithm) {
+  test('test ' + algorithm + ' against test vectors', function (t) {
+
+    vectors.forEach(function (obj, i) {
+      var input = new Buffer(obj.input, 'base64')
+      var node = obj[algorithm]
+      var js = createHash(algorithm).update(input).digest('hex')
+      t.equal(js, node, algorithm + '(testVector['+i+']) == ' + node)
+    })
+
     encodings.forEach(function (encoding) {
-        assertSame(algorithm + ' hash using ' + encoding, function (crypto, cb) {
-            cb(null, crypto.createHash(algorithm).update('hellø', 'utf-8').digest(encoding));
+        vectors.forEach(function (obj, i) {
+          var input = new Buffer(obj.input, 'base64').toString(encoding)
+          var node = obj[algorithm]
+          var js = createHash(algorithm).update(input, encoding).digest('hex')
+          t.equal(js, node, algorithm + '(testVector['+i+'], '+encoding+') == ' + node)
         })
-
-        assertSame(algorithm + ' hmac using ' + encoding, function (crypto, cb) {
-            cb(null, crypto.createHmac(algorithm, 'secret').update('hellø', 'utf-8').digest(encoding))
-        })
     });
 
-    assertSame(algorithm + ' with raw binary', function (crypto, cb) {
-        var seed = 'hellø';
-        for (var i = 0; i < 1000; i++) {
-            seed = crypto.createHash(algorithm).update(new Buffer(seed)).digest('binary');
-        }
-        cb(null, crypto.createHash(algorithm).update(new Buffer(seed)).digest('hex'));
-    });
-
-    assertSame(algorithm + ' empty string', function (crypto, cb) {
-        cb(null, crypto.createHash(algorithm).update('').digest('hex'));
-    });
+    t.end()
+  })
 });
 
--- test/create-hmac.js
+++ test/create-hmac.js
@@ -3,32 +3,45 @@ var same = require('./util').same
 var join = require('./util').join
 var test = require('tape')
 
-var data = require('crypto').pseudoRandomBytes(256)
+var algorithms = require('../').getHashes()
+var vectors = require('hash-test-vectors/hmac')
+var createHmac = require('../create-hmac')
 
-;['sha1', 'sha256', 'md5'].forEach(function (alg) {
-  console.log('alg', alg)
-  ;['p455w0rd', 'secretz', 'whatevs', 'such secure, wow', ''].forEach(function (pass) {
-    same('createHmac('+alg+', ' + JSON.stringify(pass) + ')' , function (crypto, cb) {
-      var r = crypto
-        .createHmac(alg, new Buffer(pass, 'ascii'))
-        .digest('hex')
-      console.log(r)
-      cb(null, r)
+algorithms.forEach(function (alg) {
+
+  test('hmac('+alg+')', function (t) {
+    vectors.forEach(function (input, i) {
+      var output = createHmac(alg, new Buffer(input.key, 'hex'))
+          .update(input.data, 'hex').digest()
+
+      output = input.truncate ? output.slice(0, input.truncate) : output
+      t.equal(output.toString('hex'), input[alg])
     })
+    t.end()
   })
 
-  for(var i = 8; i < data.length; i += 7)
-    (function (i) {
-      var pass = data.slice(0, i)
-      same('createHmac('+alg+', pseudoRandomBytes('+ i + ')' , function (crypto, cb) {
-        var r = crypto
-          .createHmac(alg, pass)
-          .digest('hex')
-        console.log(r)
-        cb(null, r)
-      })
-    }(i))
-
+//  ;['p455w0rd', 'secretz', 'whatevs', 'such secure, wow', ''].forEach(function (pass) {
+//    same('createHmac('+alg+', ' + JSON.stringify(pass) + ')' , function (crypto, cb) {
+//      var r = crypto
+//        .createHmac(alg, new Buffer(pass, 'ascii'))
+//        .digest('hex')
+//      console.log(r)
+//      cb(null, r)
+//    })
+//  })
+//
+//  for(var i = 8; i < data.length; i += 7)
+//    (function (i) {
+//      var pass = data.slice(0, i)
+//      same('createHmac('+alg+', pseudoRandomBytes('+ i + ')' , function (crypto, cb) {
+//        var r = crypto
+//          .createHmac(alg, pass)
+//          .digest('hex')
+//        console.log(r)
+//        cb(null, r)
+//      })
+//    }(i))
+//
 
 })
 
