72336c9fc0df931c3199ee07d84d1faa4c1b2b77
Dominic Tarr <dominic.tarr@gmail.com>
2014-09-26 00:00:57 +0300

    catch horrible mistakes that break the rng, like if it returns all zeros

--- test/random-bytes.js
+++ test/random-bytes.js
@@ -13,4 +13,29 @@ test('randomBytes', function (t) {
   });
 });
 
+test('randomBytes seem random', function (t) {
+
+  var L = 1000
+  var b = crypto.randomBytes(L)
+
+  var mean = [].reduce.call(b, function (a, b) { return a + b}, 0) / L
+
+  // test that the random numbers are plausably random.
+  // Math.random() will pass this, but this will catch
+  // terrible mistakes such as this blunder:
+  // https://github.com/dominictarr/crypto-browserify/commit/3267955e1df7edd1680e52aeede9a89506ed2464#commitcomment-7916835
+
+  // this doesn't check that the bytes are in a random *order*
+  // but it's better than nothing.
+
+  var expected = 256/2
+  var smean = Math.sqrt(mean)
+
+  t.ok(mean < expected + smean)
+  t.ok(mean > expected - smean)
+
+  t.end()
+
+})
+
 
