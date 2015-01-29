260a04134372c1d6cc3564295b2cf61bb3faa0ac
Forbes Lindesay <forbes.lindesay@red-gate.com>
2015-01-29 12:08:53 +0000

    Modularize pbkdf2-compat

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ exports.getHashes = function () {
   return hashes;
 }
 
-var p = require('./pbkdf2')(exports)
+var p = require('pbkdf2-compat')
 exports.pbkdf2 = p.pbkdf2
 exports.pbkdf2Sync = p.pbkdf2Sync
 
