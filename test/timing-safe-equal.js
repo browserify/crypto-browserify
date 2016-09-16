9767b7bc05773b1c4df85cfa8cba966abd2aa7ad
Calvin Metcalf <cmetcalf@appgeo.com>
2016-09-16 09:16:28 -0400

    use buffer shims

--- test/timing-safe-equal.js
+++ test/timing-safe-equal.js
@@ -1,28 +1,29 @@
 var test = require('tape')
 var timingSafeEqual = require('timing-safe-equal/browser')
+var bufferShims = require('buffer-shims')
 test('timingSafeEqual', function (t) {
   t.plan(5)
   t.strictEqual(
-    timingSafeEqual(Buffer.from('foo'), Buffer.from('foo')),
+    timingSafeEqual(bufferShims.from('foo'), bufferShims.from('foo')),
     true,
     'should consider equal strings to be equal'
   )
 
   t.strictEqual(
-    timingSafeEqual(Buffer.from('foo'), Buffer.from('bar')),
+    timingSafeEqual(bufferShims.from('foo'), bufferShims.from('bar')),
     false,
     'should consider unequal strings to be unequal'
   )
 
   t.throws(function () {
-    timingSafeEqual(Buffer.from([1, 2, 3]), Buffer.from([1, 2]))
+    timingSafeEqual(bufferShims.from([1, 2, 3]), bufferShims.from([1, 2]))
   }, 'should throw when given buffers with different lengths')
 
   t.throws(function () {
-    timingSafeEqual('not a buffer', Buffer.from([1, 2]))
+    timingSafeEqual('not a buffer', bufferShims.from([1, 2]))
   }, 'should throw if the first argument is not a buffer')
 
   t.throws(function () {
-    timingSafeEqual(Buffer.from([1, 2]), 'not a buffer')
+    timingSafeEqual(bufferShims.from([1, 2]), 'not a buffer')
   }, 'should throw if the second argument is not a buffer')
 })
