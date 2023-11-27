797455f7ac00c4a1cd851e916dc545cd20b7288f
Jordan Harband <ljharb@gmail.com>
2023-11-27 11:57:34 -0800

    [Tests] node 17 doesn’t support rmd160

--- test/create-hash.js
+++ test/create-hash.js
@@ -1,35 +1,44 @@
 'use strict';
 
 var test = require('tape');
+var satisfies = require('semver/functions/satisfies');
 
 var algorithms = ['sha1', 'sha224', 'sha256', 'sha384', 'sha512', 'md5', 'rmd160'];
 var encodings = ['hex', 'base64']; // FIXME: test binary
 var vectors = require('hash-test-vectors');
 
 function runTest(name, createHash, algorithm) {
-	test(name + ' test ' + algorithm + ' against test vectors', function (t) {
-		vectors.forEach(function (obj, i) {
-			var input = new Buffer(obj.input, 'base64');
-			var node = obj[algorithm];
-			var js = createHash(algorithm).update(input).digest('hex');
-			t.equal(js, node, algorithm + '(testVector[' + i + ']) == ' + node);
+	var isUnsupported = satisfies(process.version, '^17') && (
+		algorithm === 'rmd160'
+		|| algorithm === 'hmac(rmd160)'
+	);
+	test(
+		name + ' test ' + algorithm + ' against test vectors',
+		{ skip: isUnsupported && 'this node version does not support ' + algorithm },
+		function (t) {
+			vectors.forEach(function (obj, i) {
+				var input = new Buffer(obj.input, 'base64');
+				var node = obj[algorithm];
+				var js = createHash(algorithm).update(input).digest('hex');
+				t.equal(js, node, algorithm + '(testVector[' + i + ']) == ' + node);
 
-			encodings.forEach(function (encoding) {
-				var eInput = new Buffer(obj.input, 'base64').toString(encoding);
-				var eNode = obj[algorithm];
-				var eJS = createHash(algorithm).update(eInput, encoding).digest('hex');
-				t.equal(eJS, eNode, algorithm + '(testVector[' + i + '], ' + encoding + ') == ' + eNode);
+				encodings.forEach(function (encoding) {
+					var eInput = new Buffer(obj.input, 'base64').toString(encoding);
+					var eNode = obj[algorithm];
+					var eJS = createHash(algorithm).update(eInput, encoding).digest('hex');
+					t.equal(eJS, eNode, algorithm + '(testVector[' + i + '], ' + encoding + ') == ' + eNode);
+				});
+				input = new Buffer(obj.input, 'base64');
+				node = obj[algorithm];
+				var hash = createHash(algorithm);
+				hash.end(input);
+				js = hash.read().toString('hex');
+				t.equal(js, node, algorithm + '(testVector[' + i + ']) == ' + node);
 			});
-			input = new Buffer(obj.input, 'base64');
-			node = obj[algorithm];
-			var hash = createHash(algorithm);
-			hash.end(input);
-			js = hash.read().toString('hex');
-			t.equal(js, node, algorithm + '(testVector[' + i + ']) == ' + node);
-		});
 
-		t.end();
-	});
+			t.end();
+		}
+	);
 }
 
 function testLib(name, createHash) {
